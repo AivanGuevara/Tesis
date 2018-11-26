211d03987f4892d94a07877d6e0dc3080a0fd594
DuilioDeangeli <duiliodeangeli37@gmail.com>
2018-11-26 11:21:45 -0300

    Actualizacion del repo

--- build_model.py
+++ build_model.py
@@ -4,9 +4,9 @@ from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D
 from keras.layers.normalization import BatchNormalization
 import json
 
-img_w = 512
-img_h = 512
-n_labels = 4
+img_w = 256
+img_h = 256
+n_labels = 2
 
 kernel = 3
 
@@ -18,19 +18,6 @@ encoding_layers = [
     BatchNormalization(),
     Activation('relu'),
     MaxPooling2D(),
-    
-#-----------------------------------------------------------------------------------------
-#cnn_model = Sequential([
-#    Convolution2D(32,3,3,border_mode ='same',activation='relu',input_shape = input_shape),
-#    Convolution2D(32,3,3,activation='relu'),
-#    MaxPooling2D(pool_size=(2,2)) ,
-#    Dropout(0.5),
-#    Flatten(), 
-#    Dense(128,activation='relu'),
-#    Dropout(0.5),
-#    Dense(num_classes,activation = 'softmax')
-#])
-#---------------------------------------------------------------------------------
 
     Convolution2D(128, kernel, kernel, border_mode='same'),
     BatchNormalization(),
@@ -51,6 +38,16 @@ encoding_layers = [
     Activation('relu'),
     MaxPooling2D(),
 
+    Convolution2D(512, kernel, kernel, border_mode='same'),
+    BatchNormalization(),
+    Activation('relu'),
+    Convolution2D(512, kernel, kernel, border_mode='same'),
+    BatchNormalization(),
+    Activation('relu'),
+    Convolution2D(512, kernel, kernel, border_mode='same'),
+    BatchNormalization(),
+    Activation('relu'),
+    MaxPooling2D(),
 
     Convolution2D(512, kernel, kernel, border_mode='same'),
     BatchNormalization(),
@@ -83,7 +80,16 @@ decoding_layers = [
     BatchNormalization(),
     Activation('relu'),
 
-
+    UpSampling2D(),
+    Convolution2D(512, kernel, kernel, border_mode='same'),
+    BatchNormalization(),
+    Activation('relu'),
+    Convolution2D(512, kernel, kernel, border_mode='same'),
+    BatchNormalization(),
+    Activation('relu'),
+    Convolution2D(256, kernel, kernel, border_mode='same'),
+    BatchNormalization(),
+    Activation('relu'),
 
     UpSampling2D(),
     Convolution2D(256, kernel, kernel, border_mode='same'),
@@ -119,5 +125,5 @@ autoencoder.add(Reshape((n_labels, img_h * img_w)))
 autoencoder.add(Permute((2, 1)))
 autoencoder.add(Activation('softmax'))
 
-with open('editable_model_4l.json', 'w') as outfile:
+with open('model_5l.json', 'w') as outfile:
     outfile.write(json.dumps(json.loads(autoencoder.to_json()), indent=2))
